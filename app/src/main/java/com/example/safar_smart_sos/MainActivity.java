a13cf829386f85feb63d0311f17abd6184229dad
tanay4768 <tanay872.exwork@gmail.com>
2024-09-30 08:02:35 +0530

    improved

--- app/src/main/java/com/example/safar_smart_sos/MainActivity.java
+++ app/src/main/java/com/example/safar_smart_sos/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private Button startButton;
     public SpeechRecognizer speechRecognizer;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static final int REQUEST_RECORD_NOTIFICATION_PERMISSION = 500;
     private boolean isListening = false;
 
     @SuppressLint("HandlerLeak")
@@ -56,106 +57,26 @@ public class MainActivity extends AppCompatActivity {
                requestPermissions(new String[] {permission}, permissioncode);
            }
         startButton = findViewById(R.id.start_button);
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-
-        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
         messenger = new Messenger(new Handler(Looper.getMainLooper()) {
             @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case UPDATE_TEXTVIEW:
-                        String newText = msg.getData().getString("NEW_TEXT");
-                        // Update the TextView here
-                        TextView textView = findViewById(R.id.text);
-                        textView.setText(newText);
-                        break;
-                    default:
-                        super.handleMessage(msg);
+            public void handleMessage(@NonNull Message msg) {
+                if (msg.what == UPDATE_TEXTVIEW) {
+                    String newText = msg.getData().getString("NEW_TEXT");
+                    // Update the TextView here
+                    TextView textView = findViewById(R.id.text);
+                    textView.setText(newText);
+                } else {
+                    super.handleMessage(msg);
                 }
-            }
-        });
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int i) {
-
-            }
-
-            @Override
-            public void onResults(Bundle bundle) {
-                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                assert data != null;
-                String current = data.get(0);
-                if(current.contains("help")){
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        NotificationChannel channel = new NotificationChannel("sos_channel", "SOS Channel", NotificationManager.IMPORTANCE_DEFAULT);
-                        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                        manager.createNotificationChannel(channel);
-                        Notification notification = new Notification.Builder(MainActivity.this, "sos_channel")
-                                .setContentTitle("SOS Service")
-                                .setContentText("SOS called")
-                                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                                .build();
-                        startForeground(1, notification);
-                    } else {
-                        Notification notification = new Notification.Builder(MainActivity.this)
-                                .setContentTitle("SOS Service")
-                                .setContentText("SOS called")
-                                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                                .build();
-                        startForeground(1, notification);
-                    }
-                }
-                speechRecognizer.cancel();
-                speechRecognizer.startListening(speechRecognizerIntent);
-            }
-
-            private void startForeground(int i, Notification notification) {
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-
             }
         });
         startButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_RECORD_NOTIFICATION_PERMISSION);
                 } else {
                     if (!isListening) {
 //                        speechRecognizer.startListening(speechRecognizerIntent);
